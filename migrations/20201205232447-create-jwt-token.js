fdf2dad75900f16268448ba7b6320cda2ef659d4
Jérémy Halin <jeremy.halin@gmail.com>
2020-12-06 13:02:26 +0100

    :sparkles: JwtToken model

---
+++ migrations/20201205232447-create-jwt-token.js
@@ -0,0 +1,32 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("JwtTokens", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      token: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("JwtTokens");
+  },
+};
