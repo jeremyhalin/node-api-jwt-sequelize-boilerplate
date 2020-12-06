98c423c7c36dfcc6516e12985392a66bce1e132e
Jérémy Halin <jeremy.halin@gmail.com>
2020-12-06 13:03:06 +0100

    :recycle: Refact Sequelize inti config

--- models/index.js
+++ models/index.js
@@ -8,12 +8,7 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config
-);
+const sequelize = new Sequelize(config);
 
 sequelize
   .authenticate()
