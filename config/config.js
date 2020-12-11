ae68b1241c65459b97c6cb2760a97749bafb824a
Jérémy Halin <jeremy.halin@gmail.com>
2020-12-11 09:34:13 +0100

    :construction_worker: Test env

--- config/config.js
+++ config/config.js
@@ -10,12 +10,12 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
+    username: "root",
+    password: "",
+    database: "node-api-boilerplate-test",
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
   },
   production: {
     username: process.env.DB_USERNAME,
