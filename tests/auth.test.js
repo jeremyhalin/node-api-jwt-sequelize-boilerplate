6a9f2d29484597fd5918e4a90215aabb1979b3f0
Jérémy Halin <jeremy.halin@gmail.com>
2020-12-11 20:07:00 +0100

    :white_check_mark: Fix Auth tests

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,64 +1,48 @@
+const { StatusCodes } = require("http-status-codes");
 const request = require("supertest");
-const HttpStatus = require("http-status-codes");
 const app = require("../app");
 
 const email = "demo@demo.com";
 const password = "demo";
-const firstname = "Hubert";
-const lastname = "Bonisseur";
 
-describe("Auth Endpoints", () => {
-  test("create a new user", async (done) => {
-    const response = await request(app).post("/api/v1/auth/register").send({
-      email,
-      password,
-      firstname,
-      lastname,
-    });
-
-    expect(response.body.data.type).toBe("user");
-    expect(response.body.data.id).toBeDefined();
-    expect(response.body).toHaveProperty("included");
-    expect(response.body.included[0].type).toBe("token");
-    expect(response.body.included[1].type).toBe("refresh_token");
-    expect(response.status).toBe(HttpStatus.CREATED);
-    done();
+describe("POST /auth/register", function () {
+  test("register a user", function (done) {
+    request(app)
+      .post("/api/v1/auth/register")
+      .send({ email, password })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect((res) => {
+        expect(res.body.data.type).toBe("user");
+        expect(res.body.data).toHaveProperty("id");
+        expect(res.body.data.attributes.email).toBe(email);
+        expect(res.body.data.attributes.role).toBe("Guest");
+      })
+      .expect(201)
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
   });
+});
 
-  test("login a user", async (done) => {
-    const response = await request(app).post("/api/v1/auth/login").send({
-      email,
-      password,
-    });
-    expect(response.body.data.type).toBe("user");
-    expect(response.body.data.attributes.email).toBeDefined();
-    expect(response.body.data.id).toBeDefined();
-    expect(response.status).toBe(HttpStatus.OK);
-    done();
-  });
-
-  test("login fails with invalid credentials", async (done) => {
-    const response = await request(app).post("/api/v1/auth/login").send({
-      email,
-      password: "wrongpassword",
-    });
-    expect(response.status).toBe(HttpStatus.UNAUTHORIZED);
-    done();
-  });
-
-  test("login fails with missing email", async (done) => {
-    const response = await request(app).post("/api/v1/auth/login").send({
-      password,
-    });
-    expect(response.status).toBe(HttpStatus.BAD_REQUEST);
-    done();
-  });
-
-  test("login fails with missing password", async (done) => {
-    const response = await request(app).post("/api/v1/auth/login").send({
-      email,
-    });
-    expect(response.status).toBe(HttpStatus.BAD_REQUEST);
-    done();
+describe("POST /auth/login", function () {
+  test("log in a user", function (done) {
+    request(app)
+      .post("/api/v1/auth/login")
+      .send({ email, password })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect((res) => {
+        expect(res.body.data.type).toBe("user");
+        expect(res.body.data).toHaveProperty("id");
+        expect(res.body.data.attributes.email).toBe(email);
+        expect(res.body.data.attributes.role).toBe("Guest");
+      })
+      .expect(StatusCodes.OK)
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
   });
 });
